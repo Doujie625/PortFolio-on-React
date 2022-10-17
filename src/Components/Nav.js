3c1d235d58823e293a4a7571bd5db5d89c64bcdb
Devin Jansa <devin_jansa@icloud.com>
2022-10-17 13:10:45 -0700

    fixed somne responsive errors

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,7 +5,6 @@ import { links } from "./Nav_data";
 import { useState } from "react";
 
 const Nav = function (props) {
-    console.log(props)
     const [navColor, setNavColor] = useState("")
 
     function setNavBarColor(e) {
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,19 +1,44 @@
 import CSS from "./../CSS/Projects.css";
 import "./../CSS/style.css"
 import { projectData } from "./ProjectData";
+import { useEffect, useRef, useState } from "react";
 
-const Projects = function () {
+
+var Projects = function (props) {
+  const [slide, setSlide] = useState(1);
+
+  const next_slide = () => {
+    if (slide >= 3) {
+      return
+    }
+    setSlide(slide + 1)
+  }
+  const prev_slide = () => {
+    if (slide <= 1) {
+      
+      return
+    }
+    setSlide(slide - 1)
+  }
+
+    console.log(slide)
   return (
     <div className="Projects" id="Projects">
-
       <div className="Projects_top" id="Top">
         <p className="neonTextPink">Projects</p>
       </div>
 
-      <div className="Projects_bottom">
+      <div className="Projects_bottom" >
+      <div className="left_button" onClick={prev_slide}>
+          <p className="neonTextBlue" id="left_arrow">
+            V
+          </p>
+        </div>
+
         {projectData.map((link) => {
-          if (link.text == "JellyFish") {
-            return (
+          if (props.deviceSize == "small") {
+            if (link.id == slide && link.text == "JellyFish") {            
+              return (
               <div key={link.id} className="carousel_item">
                 <a href={link.url} key={link.id} className={link.class}>
                   <div id="sprite-icon">
@@ -24,20 +49,57 @@ const Projects = function () {
                 </a>
               </div>
             );
-          } else {
-            return (
-              <div key={link.id} className="carousel_item">
-                <a href={link.url} key={link.id} className={link.class}>
-                  <img id="icons" src={link.icon} />
-                  <br/>
-                  <br/>
-                  {link.text}
-                </a>
-              </div>
-            );
+            } else if (link.id == slide) {
+              return (
+                <div key={link.id} className="carousel_item">
+                  <a href={link.url} key={link.id} className={link.class}>
+                    <img id="icons" src={link.icon} />
+                    <br/>
+                    <br/>
+                    {link.text}
+                  </a>
+                </div>
+              );
             }
-          })}
+          }
+          if (props.deviceSize != "small") {
+            if (link.text == "JellyFish") {
+              return (
+                <div key={link.id} className="carousel_item">
+                  <a href={link.url} key={link.id} className={link.class}>
+                    <div id="sprite-icon">
+                      <div className="pink" id="sprite-image">
+                      </div>
+                    </div>
+                    {link.text}
+                  </a>
+                </div>
+              );
+            } else {
+              return (
+                <div key={link.id} className="carousel_item">
+                  <a href={link.url} key={link.id} className={link.class}>
+                    <img id="icons" src={link.icon} />
+                    <br/>
+                    <br/>
+                    {link.text}
+                  </a>
+                </div>
+              );
+            }
+          }
+
+        })}
+
+<div className="right_button" onClick={next_slide}>
+          <p className="neonTextBlue" id="right_arrow">
+            V
+          </p>
+        </div>
+
       </div>
+
+
 
     </div>
   );
